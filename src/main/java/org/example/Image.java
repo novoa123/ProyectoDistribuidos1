c0cc2a4b124fc9a7c166c520f32b78d1ea917600
Gustavo <gtorres21@alumnos.utalca.cl>
2025-09-29 20:06:19 -0300

    image loco

--- src/main/java/org/example/Image.java
+++ src/main/java/org/example/Image.java
@@ -7,8 +7,8 @@ import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
-// Clase para manejar lectura, conversion y escritura de imágenes
-// Maneja la conversion a escala de grises y RGB
+// clase para manejar lectura y  conversion de las imagenes
+// se maneja la conversion RGB
 
 public class Image {
 
@@ -28,22 +28,6 @@ public class Image {
     }
 
 
-    public int[][] toMatrizGris(){
-        int[][] matriz = new int[largo][ancho];
-        
-        for (int y = 0; y < largo; y++) {
-            for (int x = 0; x < ancho; x++) {
-                int rgb = image.getRGB(x, y);
-                int r = (rgb >> 16) & 0xFF;
-                int g = (rgb >> 8) & 0xFF;
-                int b = rgb & 0xFF;
-
-                matriz[y][x] = (int)(0.299 * r + 0.587 * g + 0.114 * b);
-            }
-        }
-        return matriz;
-    }
-
     public int[][][] toMatrizRGB(){
         int[][][] rgbMatriz = new int[3][largo][ancho];
 
@@ -59,19 +43,6 @@ public class Image {
         return rgbMatriz;
     }
 
-    public void matrizGrisAImagen(int[][] matriz, String outputPath) throws IOException {
-        BufferedImage output = new BufferedImage(ancho, largo, BufferedImage.TYPE_INT_RGB);
-
-        for (int y = 0; y < largo; y++) {
-            for (int x = 0; x < ancho; x++) {
-                int gris = Math.max(0, Math.min(255, matriz[y][x]));
-                int rgb = (gris << 16) | (gris << 8) | gris;
-                output.setRGB(x, y, rgb);
-            }
-        }
-
-        ImageIO.write(output, "png", new File(outputPath));
-    }
 
     public void matrizRGBaImagen(int[][][] matriz, String outputPath) throws IOException {
         BufferedImage output = new BufferedImage(ancho, largo, BufferedImage.TYPE_INT_RGB);
